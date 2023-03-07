aa3c5011093d82719e673f9ecf21bedc6f8ac903
asaad227 <a.saad227@yahoo.com>
2023-03-07 19:09:20 +0000

    css update

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const players2S = document.querySelector(".item1");
 const playerWin = document.querySelector(".leader");
 var round = document.querySelector(".gameRound");
 var roundIn = document.querySelector(".roundInput");
-
 var square;
 var result = []
 var result2 = []
@@ -25,10 +24,7 @@ function roundEnter(){
   roundIn.value = ""
 }
 
-
-
 function submitName(){
-    // cheeseBoard()
     var str = nameInput.value;
     //multiple replace in a string 
     str = str.replace(/[aA]/gi, "4");
@@ -42,8 +38,7 @@ function submitName(){
     
 }
 
-function submitName1(){
-   
+function submitName1(){ 
     var str = nameInput2.value;
     str = str.replace(/[aA]/gi, "4");
     str = str.replace(/[eE]/gi, "3");
@@ -120,81 +115,8 @@ function cheeseBoard1(){
       }
       
     }
-
-
-
 }
-// function winner2(){
-// const yellow2 = result2.filter(x => x === "yellow");
-// const green2 = result2.filter(x=> x=== "green");
-// const blue2 = result2.filter(x=> x === "blue");
-// const red2 = result2.filter(x=> x === "red");
-// var re = ""
 
-// if(blue2.length  > green2.length  && blue2.length  > yellow2.length  && blue2.length  > red2.length){
-//   re = "Blue";
-// }
-// if(yellow2.length > green2.length && yellow2.length > red2.length && yellow2.length > blue2.length){
-//   re = "Yellow";
-// }
-
-// if(green2.length  > blue2.length  && green2.length  > yellow2.length  && green2.length  > red2.length){
-//   re = "Green";
-// }
-// if(red2.length  > green2.length  && red2.length  > yellow2.length  && red2.length  > blue2.length){
-//   re = "Red"
-// }
-// const name2 = document.querySelector(".playerName1").innerHTML;
-
-// const user2 = { 
-//   // as we going to send name only nothing else so we slice first 12 character from 
-//   // the player innerHTML only name sent to localstorage
-//   name:`${name2.slice(13)}`,
-//   score: `${re}`,
-// };
-// localStorage.setItem("user2", JSON.stringify(user2));
-// leaderBoard2.push(re);
-// // document.querySelector(".win2").value = `${re}`
-// }
-// function winner(){
-// const yellow = result.filter(x => x === "yellow");
-// const green = result.filter(x=> x=== "green");
-// const blue = result.filter(x=> x === "blue");
-// const red = result.filter(x=> x === "red");
-
-
-// var re = ""
-
-
-// if(yellow.length > green.length && yellow.length > red.length && yellow.length > blue.length){
-//   re = "Yellow"
-// } 
-// if(blue.length  > green.length  && blue.length  > yellow.length  && blue.length  > red.length){
-//     re = "Blue"
-// }
-// if(green.length  > blue.length  && green.length  > yellow.length  && green.length  > red.length){
-//     re = "Green"
-// } 
-// if(red.length  > green.length  && red.length  > yellow.length  && red.length  > blue.length){
-//     re = "Red"
-// }
-// //here we store the name into variable 
-// const namel = player.innerHTML;
-
-// const user1 = { 
-//     // as we going to send name only nothing else so we slice first 12 character from 
-//     // the player innerHTML only name sent to localstorage
-//     name: `${namel.slice(13)}`,
-//     score: `${re}`,
-//   };
-
- 
-  
-//   localStorage.setItem("user1", JSON.stringify(user1));
- 
-//    leaderBoard.push(re)
-//   //  document.querySelector(".win").value = `${re}`
-// }
 function gameScore1(){
   for(let i = 0; i < result.length; i++){
     if(result[i] === "red"){
@@ -236,21 +158,11 @@ function gameScore2(){
 function leader(){
   gameScore1()
   gameScore2()
-    // winner()
-    // winner2()
     if(score > score2){
       playerWin.innerHTML = `Winner: Player1`
     }else{
       playerWin.innerHTML = `Winner: Player2`
-    }
-// const storedUser = JSON.parse(localStorage.getItem("user1"));
-// document.querySelector(".playerName").innerHTML = `Player name1: ${storedUser.name}`;
-// document.querySelector(".item").innerHTML = `Player score1: ${storedUser.score}`;
-// const storedUser1 = JSON.parse(localStorage.getItem("user2"));
-// document.querySelector(".playerName1").innerHTML = `Player name2:${storedUser1.name}`;
-// document.querySelector(".item1").innerHTML = `Player score2:${storedUser1.score}`;
-
-   
+    }   
 }
 
 
@@ -276,7 +188,6 @@ const calculateWinner = document.querySelector(".result");
 const submit = document.querySelector(".nameSub");
 const submit2 = document.querySelector(".nameSub2");
 const re = document.querySelector(".reset");
-
 roundBtn.addEventListener("click", roundEnter);
 calculateWinner.addEventListener("click", leader);
 submit.addEventListener("click", submitName )
